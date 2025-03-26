13f5f473de2f526aa63d1950a3a3c1a69546804b
Diogo Silva <diogo.silva@five9.com>
2025-03-26 01:14:54 +0000

    fix: export mask_string

--- maskerlogger/__init__.py
+++ maskerlogger/__init__.py
@@ -2,6 +2,17 @@
 Init file for oxformatter package.
 """
 
-from maskerlogger.masker_formatter import MaskerFormatter, MaskerFormatterJson  # noqa
+from maskerlogger.masker_formatter import (
+    MaskerFormatter,
+    MaskerFormatterJson,
+    mask_string,
+)
+
+# Expose the classes and main function
+__all__ = [
+    "MaskerFormatter",
+    "MaskerFormatterJson",
+    "mask_string",
+]
 
 __version__ = "0.4.0-beta.1"
