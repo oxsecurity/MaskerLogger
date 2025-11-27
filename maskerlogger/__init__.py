e2f857b0abe2aa90c858239bad3c83c47f7c126e
reuvenshirazi <reuven@ox.security>
2025-11-27 14:01:53 +0200

    patch version

--- maskerlogger/__init__.py
+++ maskerlogger/__init__.py
@@ -4,4 +4,4 @@ Init file for maskerlogger package.
 
 from maskerlogger.masker_formatter import MaskerFormatter, MaskerFormatterJson, SKIP_MASK  # noqa
 
-__version__ = "1.1.0"
+__version__ = "1.1.1"
